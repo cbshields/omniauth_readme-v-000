b806cf9fecdd9abe301f5c3cc31c5fa873ecec6e
Caroline Shields <cbshields@outlook.com>
2018-05-01 20:29:03 +0000

    automatically backed up by learn

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
-    
+    session[:user_id] = @user.id
+
+    render 'welcome/home'
   end
 
 end
